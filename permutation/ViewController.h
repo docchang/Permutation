1e0c9804749fe9ba3aa1d6796b0ae00b74fc58a7
Dominic Chang <doc.dominic@gmail.com>
2012-01-03 15:15:25 +0800

    Initial Commit

---
+++ permutation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  permutation
+//
+//  Created by Dominic Chang on 1/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
